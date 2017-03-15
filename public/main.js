6090dc0705866878673ddf0119fbe4cb17111221
Izabela Louw <izabelabraga@hotmail.com>
2017-03-15 15:30:14 -0600

    Commited

--- public/main.js
+++ public/main.js
@@ -7,44 +7,52 @@ $('document').ready(function(){
  $(".city").click(function(event){
    var value = $(this).data("loc");
    cityLocal(value);
+  //  console.log(value);
    // This function will give me back the search of cities
  });
 });
 
 function starLocal(value){
-  console.log("This is the Value/Stars: "+ value);
+  // console.log("This is the Value/Stars: "+ value);
 
 var url_1 = "http://apir.viamichelin.com/apir/2/findPoi.json2/RESTAURANT/eng?center=-87.63:41.89&nb=100&dist=10000000&source=RESGR&filter=michelin_stars%20eq%20"+value+"%20&charset=UTF-8&ie=UTF-8&authKey=RESTGP20170310222038934049903020";
-console.log(url_1);
+// console.log(url_1);
 var url_2 = "http://apir.viamichelin.com/apir/2/findPoi.json2/RESTAURANT/eng?center=-73.87:40.72&nb=100&dist=10000000&source=RESGR&filter=michelin_stars%20eq%20"+value+"%20&charset=UTF-8&ie=UTF-8&authKey=RESTGP20170310222038934049903020";
 
 var url_3 = "http://apir.viamichelin.com/apir/2/findPoi.json2/RESTAURANT/eng?center=-122.41:37.77&nb=100&dist=10000000&source=RESGR&filter=michelin_stars%20eq%20"+value+"%20&charset=UTF-8&ie=UTF-8&authKey=RESTGP20170310222038934049903020";
 
-  console.log("This is the API URL: "+ url_1);
+var json1;
+var json2;
+  // console.log("This is the API URL: "+ url_1);
 $.get(url_1)
 .then(function(data) {
-  var json1 = JSON.parse(data);
+   json1 = JSON.parse(data);
   return $.get(url_2);
 }).then(function(data) {
-  var json2 = JSON.parse(data);
+   json2 = JSON.parse(data);
   return $.get(url_3);
 }).then (function(data){
   var json = JSON.parse(data);
   appendResult(json);
   appendResult(json1);
   appendResult(json2);
+  // console.log(data);
 });
-
 }
+
 function cityLocal(value){
-  console.log("This is the Value/Lat and Long: "+ value);
-  var url = "http://apir.viamichelin.com/apir/2/findPoi.json2/RESTAURANT/eng?center=" + value + "&nb=100&dist=10000000&source=RESGR&filter=michelin_stars%20ge%201%20&charset=UTF-8&ie=UTF-8&authKey=RESTGP20170310222038934049903020"
-    console.log("This is the API URL: "+ url);
+  console.log(value);
+  var url = "http://apir.viamichelin.com/apir/2/findPoi.json2/RESTAURANT/eng?center=-87.63:41.89&nb=100&dist=10000000&source=RESGR&filter=michelin_stars%20ge%201%20&charset=UTF-8&ie=UTF-8&authKey=RESTGP20170310222038934049903020"
+    //  console.log(url);
   $.get(url)
+
   .then(function(data) {
     var json = JSON.parse(data);
-    appendResult(json);
-  })
+     appendResult(json);
+
+  }).catch(function(error){
+console.log(error)
+  });
 }
 function appendResult(json){
   // console.log("I am parsed data", json)
@@ -70,12 +78,16 @@ function appendResult(json){
     }
     //Return the amount of stars.
 
-    console.log( description);
+    // console.log( description);
 
-    var htmltoAppend = '<article class ="restaurant row"><div class ="resPic col-md-4"> <img src="'+image+'" alt="" class="img-thumbnail"></div><div class="info col-md-3" ><ul class="detail"><li class ="restNames">'+name+'</li><li class ="restCity">'+cities+'</li><li class ="restWeb"><a href="http://'+web+'" target="blank">'+web+'</a></li><li class = "resCook">'+cuisine+'</li><li class = "restStar">'+stars+'</li></ul></div><div class ="restDesc col-md-5">'+description+'</div><hr class="col-md-12"></article>';
-    
 
-    list.append(htmltoAppend);
+
+      var htmltoAppend = '<article class ="restaurant row"><div class ="resPic col-md-4"> <img src="'+image+'" alt="" class="img-thumbnail"></div><div class="info col-md-3" ><ul class="detail"><li class ="restNames">'+name+'</li><li class ="restCity">'+cities+'</li><li class ="restWeb"><a href="http://'+web+'" target="blank">'+web+'</a></li><li class = "resCook">'+cuisine+'</li><li class = "restStar">'+stars+'</li></ul></div><div class ="restDesc col-md-5">'+description+'</div><hr class="col-md-12"></article>';
+
+        list.append(htmltoAppend);
+      
+
+
 
     // console.log("Rest Name is: " + name);
     //$(".restNames").append(name,description)
